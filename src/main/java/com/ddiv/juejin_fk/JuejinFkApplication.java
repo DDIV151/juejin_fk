c9a7d326bb838b5355c84ad71ca01d2f4c0cfc53
DDIV151 <ddiv151@163.com>
2025-01-29 16:26:16 +0800

    Initial commit

---
+++ src/main/java/com/ddiv/juejin_fk/JuejinFkApplication.java
@@ -0,0 +1,13 @@
+package com.ddiv.juejin_fk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuejinFkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuejinFkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ddiv/juejin_fk/JuejinFkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddiv.juejin_fk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuejinFkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
